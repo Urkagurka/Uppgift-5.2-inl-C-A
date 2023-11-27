5b7e83a1cf1bb5b5bd7d09481758c08ea33e9f4a
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2023-11-27 19:55:46 +0100

    Add project files.

---
+++ Uppgift 5.2 inl C-A/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uppgift_5._2_C_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många heltal vill du skriva in?");
+            int antal = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in dina heltal på olika rader");
+            int[] siffror = new int[antal];
+            for (int i = 0; i < antal; i++)
+            {
+                siffror[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Här är talen du skrev in");
+            for (int i = 0; i < antal; i++)
+            {
+                Console.WriteLine("TAL " + (i+1) + " :" + siffror[i]);
+            }
+        }
+    }
+}
